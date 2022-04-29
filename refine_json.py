9e2ff18a3217ffc379cf3238bcfc84cef615a768
shangjie-li <1814481684@qq.com>
2022-04-29 16:08:23 +0800

    Read the shape of the image automatically

--- refine_json.py
+++ refine_json.py
@@ -30,6 +30,7 @@ class json2json(object):
         
     def save_json(self):
         labelme_json = glob.glob(os.path.join(self.input_root, '*.json'))
+        labelme_json.sort()
         num = len(labelme_json)
         for idx, json_file in enumerate(labelme_json):
             print('Processing: %d / %d...' % (idx + 1, num))
@@ -40,14 +41,26 @@ class json2json(object):
                 for shape in data['shapes']:
                     shape['line_color'] = None
                     shape['fill_color'] = None
-                    del shape['group_id']
+                    if 'group_id' in shape:
+                        del shape['group_id']
                 
                 data['lineColor'] = [0, 255, 0, 128]
                 data['fillColor'] = [255, 0, 0, 128]
-                
                 data['imageData'] = None
-                data['imagePath'] = data['imagePath'].split('/')[-1]
-                del data['summary']
+                
+                if 'summary' in data:
+                    del data['summary']
+                if '/' in data['imagePath']:
+                    data['imagePath'] = data['imagePath'].split('/')[-1]
+                if data['imageHeight'] == 0 and data['imageWidth'] == 0:
+                    try:
+                        import matplotlib.pyplot as plt
+                        img_file = json_file.replace(
+                            os.path.splitext(json_file)[1], os.path.splitext(data['imagePath'])[1])
+                        img = plt.imread(img_file)
+                        data['imageHeight'], data['imageWidth'], _ = img.shape
+                    except:
+                        pass
 
             save_json_path = os.path.join(self.output_root, json_file.split('/')[-1])
             print('Saving output json file to: %s' % save_json_path)
@@ -58,6 +71,7 @@ if __name__ == '__main__':
     parse_args()
     print('Input root directory:\n %s' % Path(args.input_root).resolve())
     print('Output root directory:\n %s' % Path(args.output_root).resolve())
+    assert os.path.exists(args.input_root), 'Directory Not Found: %s' % args.input_root
     if not os.path.exists(args.output_root):
         os.mkdir(args.output_root)
     
